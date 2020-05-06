96eeefbb8aa8b9a0263e92c6a67f4287f8c9f295
itcast <itheima@itcast.cn>
2020-05-07 01:53:09 +0800

    20200507

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
+//import Vue from "vue/dist/vue.common.js";
 
+//导入路由的包
+import VueRouter from 'vue-router'
+//安装路由
+Vue.use(VueRouter)
+
+//导入mui
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 //按需导入mint-UI中的组件
 import { Header } from 'mint-ui'
 Vue.component(Header.name, Header)
+
+//导入自己的router
+import router from './router.js'
 
 //导入app根组件
 import app from './app.vue'
@@ -12,5 +23,6 @@ import app from './app.vue'
 var vm = new Vue({
   el: '#app',
   data: {},
-  render: c => c(app)
+  render: c => c(app),
+  router  //挂载路由对象
 })
